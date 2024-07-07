af0a9ab3e397a4794ffdc5ee9b373dc3d788c479
pythoncloudminer <141788762+pythoncloudminer@users.noreply.github.com>
2024-07-07 18:36:58 +0530

    Adding some list modification function
    List modification function to create nested list.

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ print("Testing started for CI/CD pipeline!!!")
 
 def add(x, y):
   return x+y;
+
+
+def list_modifier(lista):
+  some_list = []
+  for i in len(lista):
+    some_list.append(lista[i:])
+
+print(list_modifier([9,2,3,45]))
+    
